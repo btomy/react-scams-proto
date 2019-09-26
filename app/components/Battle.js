2c7dc451a8d2dcc3f77e5c6e2d7d4655a41c7939
btomy <binu.tomy@citizensadvice.org.uk>
2019-09-26 12:31:52 +0100

    feature [adding boilerplate files]

---
+++ app/components/Battle.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  FaUserFriends,
+  FaFighterJet,
+  FaTrophy,
+  FaTimesCircle
+} from 'react-icons/fa';
+
+function Instructions() {
+  return (
+    <div className="instructions-container">
+      <h1 className="center-text header-lg">Instructions</h1>
+      <ol className="container-sm grid center-text battle-instructions">
+        <li>
+          <h3 className="header-sm">Enter two Github users</h3>
+          <FaUserFriends
+            className="bg-light"
+            color="rgb(255, 191, 116)"
+            size={140}
+          />
+        </li>
+        <li>
+          <h3 className="header-sm">Battle</h3>
+          <FaFighterJet className="bg-light" color="#727272" size={140} />
+        </li>
+        <li>
+          <h3 className="header-sm">See the winners</h3>
+          <FaTrophy className="bg-light" color="rgb(255, 215, 0)" size={140} />
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: ''
+    };
+
+    this._handleSubmit = this._handleSubmit.bind(this);
+    this._handleChange = this._handleChange.bind(this);
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.username);
+  }
+
+  _handleChange(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form className="column player" onSubmit={this._handleSubmit}>
+        <label htmlFor="username" className="player-label">
+          {this.props.label}
+        </label>
+        <div className="row player-inputs">
+          <input
+            className="input-light"
+            type="text"
+            id="username"
+            placeholder="Username"
+            autoComplete="off"
+            value={this.state.username}
+            onChange={this._handleChange}
+          />
+          <button
+            className="btn dark-btn"
+            type="submit"
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+};
+function PlayerPreview({ username, onReset, label }) {
+  return (
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className="row bg-light">
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`https://github.com/${username}.png?size=200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className="link">
+            {username}
+          </a>
+        </div>
+        <button className="btn-clear flex-center" onClick={onReset}>
+          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+PlayerPreview.propTypes = {
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired
+};
+
+export default class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    };
+    this._handleSubmit = this._handleSubmit.bind(this);
+    this._handleReset = this._handleReset.bind(this);
+  }
+
+  _handleSubmit(id, player) {
+    this.setState({
+      [id]: player
+    });
+  }
+  _handleReset(id) {
+    this.setState({
+      [id]: null
+    });
+  }
+
+  render() {
+    const { playerOne, playerTwo } = this.state;
+    return (
+      <React.Fragment>
+        <Instructions />
+        <div className="player-container">
+          <h1 className="center-text header-lg">Players</h1>
+          <div className="row space-round">
+            {playerOne === null ? (
+              <PlayerInput
+                label="Player One"
+                onSubmit={player => this._handleSubmit('playerOne', player)}
+              />
+            ) : (
+              <PlayerPreview
+                username={playerOne}
+                label="Player One"
+                onReset={() => this._handleReset('playerOne')}
+              />
+            )}
+
+            {playerTwo === null ? (
+              <PlayerInput
+                label="Player Two"
+                onSubmit={player => this._handleSubmit('playerTwo', player)}
+              />
+            ) : (
+              <PlayerPreview
+                username={playerTwo}
+                label="Player Two"
+                onReset={() => this._handleReset('playerTwo')}
+              />
+            )}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}

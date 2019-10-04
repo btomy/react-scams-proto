0ee049ce7bbc70d8d911e0eb36a0998f9d40e39d
binu.tomy <binu.tomy@citizensadvice.org.uk>
2019-10-04 18:23:42 +0100

    adding question and answers

--- app/Steps.js
+++ app/Steps.js
@@ -16,11 +16,23 @@ class Steps extends Component{
     render() {
         const {data} = this.state
         console.log(data);
-         
+        const FirstQuestionId = data.StartingQuestionId
+        const FirstQuestion = data ? data["Questions"].filter( item => {
+            return item.Id === FirstQuestionId
+        }): null;
+        console.log(FirstQuestion);
         return(
             <React.Fragment>
                 <h1>{data.Title}</h1>
                 <div dangerouslySetInnerHTML={{__html: data.Introduction}}></div>
+
+                <div className="questionBlock">
+                    <h3>{FirstQuestion.map( question => question.QuestionText )}</h3>
+                </div>
+
+                <div className="answerBlock">
+                    <h3>{FirstQuestion.map( question => question.Answers.map( answer => answer.Answer) )}</h3>
+                </div>
             </React.Fragment>
         )
     }

992509a4729073e9b87a153cfe24439040696050
binu.tomy <binu.tomy@citizensadvice.org.uk>
2019-10-07 12:15:32 +0100

    splitting into many components

--- app/Steps.js
+++ app/Steps.js
@@ -1,40 +1,111 @@
 import React, {Component} from "react";
 
 import data from "./scams.json"
+import FormRadioCheck from "./components/FormRadioCheck/component";
+import FormLabel from "./components/FormLabel/component";
+import Step1 from "./components/Steps/Step1.js";
+
 class Steps extends Component{
     constructor(props) {
         super(props)
         this.state = {
-            data : data
+            data : data,
+            selected : "email"
         }
     }
 
-    _handleChange = () => {
-
+    _handleChange = (e) => {
+        e.preventDefault();
+        this.setState({
+            selected: e.target.value
+        })
     }
 
     render() {
-        const {data} = this.state
-        console.log(data);
+        const {data,selected} = this.state
         const FirstQuestionId = data.StartingQuestionId
         const FirstQuestion = data ? data["Questions"].filter( item => {
             return item.Id === FirstQuestionId
         }): null;
-        console.log(FirstQuestion);
-        return(
-            <React.Fragment>
-                <h1>{data.Title}</h1>
-                <div dangerouslySetInnerHTML={{__html: data.Introduction}}></div>
+        
+        const FirstQuestionAnswers = FirstQuestion.map( question => {
+            return question.Answers.filter( answer => answer)
+        });
+        console.log(selected)
+        
+        
 
-                <div className="questionBlock">
-                    <h3>{FirstQuestion.map( question => question.QuestionText )}</h3>
-                </div>
+        return (
+          <React.Fragment>
+            <h1>{data.Title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: data.Introduction }}></div>
 
-                <div className="answerBlock">
-                    <h3>{FirstQuestion.map( question => question.Answers.map( answer => answer.Answer) )}</h3>
-                </div>
-            </React.Fragment>
-        )
+            <StepOne 
+                questions={FirstQuestion}
+                results={FirstQuestionAnswers[0]}
+                selected={selected}
+                handleChange={this._handleChange}
+                 />
+            
+          </React.Fragment>
+        );
     }
 }
 export default Steps;
+
+
+const StepOne = ({questions, results, selected, handleChange }) => {
+    return (
+      <div className="rich-content">
+        <QuestionBlock questions={questions} />
+
+        <fieldset className="p-0 m-0">
+          <Answers
+            results={results}
+            selected={selected}
+            handleChange={handleChange}
+          />
+        </fieldset>
+      </div>
+    );
+
+}
+
+
+const QuestionBlock = ({questions}) => {
+    const QuestionBlock = questions.map(question => question.QuestionText);
+    return(
+        <h3>{QuestionBlock}</h3>
+    )
+}
+
+
+const Answers = ({results , selected, handleChange}) => {
+    
+    const resultsBlock = results.map( (result, id) => {
+        return (
+          <div className="block mv-2" key={id}>
+            <div className="checkbox" >
+              <FormRadioCheck
+                type="checkbox"
+                id={result.AnswerCode}
+                name={result.AnswerCode}
+                value={result.AnswerCode}
+                checked={result.AnswerCode === selected}
+                handleChange={handleChange}
+                classNames="input"
+              />
+              <FormLabel
+                htmlFor={result.AnswerCode}
+                classNames="ml-2 lh-2 semi-bold"
+              >
+                {result.Answer}
+              </FormLabel>
+            </div>
+          </div>
+        );
+    })
+    return(
+        <React.Fragment>{resultsBlock}</React.Fragment>
+    )
+} 
